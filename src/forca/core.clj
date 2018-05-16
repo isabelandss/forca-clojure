7610f8d7f2d6162ece2037ba5d89b55c540f41e1
isabelandss <isabela.santos@fcamara.com.br>
2018-05-16 11:36:44 -0300

    comentando

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -3,10 +3,13 @@
 
 (def total-de-vidas 6)
 
+;;função que impressão
 (defn perdeu [] (print "Você perdeu"))
 
+;;função que impressão
 (defn ganhou [] (print "Você ganhou"))
 
+;;função remove as letras certas da palavra
 (defn letras-faltantes [palavra acertos]
   (remove 
     (fn [letra] (contains? acertos (str letra))) 
@@ -14,17 +17,32 @@
   )
 )
 
+;;uma vez vazio, o jogador ganhou
 (defn acertou-a-palavra-toda? [palavra acertos]
   (empty? (letras-faltantes palavra acertos))
 )
 
+;;pega entrada de dados
+(defn le-letra! [] (read-line))
+
+;;o chute tem na palavra?
+(defn acertou? [chute palavra] (.contains palavra chute))
+
+;;funcao principal do jogo
 (defn jogo [vidas palavra acertos]
-  (if (= vidas 0)  
-    (perdeu) 
-    (if (acertou-a-palavra-toda? palavra acertos)
-      (ganhou)
-      (print "Chuta, amigo! ")
+  (if (= vidas 0)  ;; se vidas é igual a zero
+    (perdeu) ;;você perdeu
+    (if (acertou-a-palavra-toda? palavra acertos) ;;senão, acertou a palavra toda?
+      (ganhou) ;;você ganhou
+      (avalia-chute (le-letra!) vidas palavra acertos) ;;senão, pegue o chute na entrada, verifique e contabilize
     )
+  )
+)
+
+(defn avalia-chute [chute vidas palavra acertos]
+  (if (acertou? chute palavra)
+    (jogo vidas palavra (conj acertos chute))
+    (jogo (dec vidas) palavra acertos)
   )
 )
     
